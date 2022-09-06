bdf93be54f5514b5080c0aab6aa507ff77fd23d1
Steven Croft <scroft001@gmail.com>
2022-09-05 21:25:06 -0700

    Alt even handler syntax

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -12,7 +12,7 @@ class SearchBar extends React.Component {
                 <form className='ui form'>
                     <div className='field'>
                         <label>Image Search</label>
-                        <input type="text" placeholder="Search" onChange={this.onInputChange} />
+                        <input type="text" placeholder="Search" onChange={(e) => console.log(e.target.value) } />
                     </div>
                 </form>
             </div>
