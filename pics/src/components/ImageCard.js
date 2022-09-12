1d663596a27f0d703d9fe54e7bfc4ac9f7709f76
Steven Croft <scroft001@gmail.com>
2022-09-11 21:52:18 -0700

    Getting client height with refs

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
 
         const {description, urls} = this.props.image;
@@ -10,6 +25,7 @@ class ImageCard extends React.Component {
                 <img 
                     alt={description}
                     src={urls.regular}
+                    ref={this.imageRef}
                 />
             </div>
         )
